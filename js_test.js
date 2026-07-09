63a017f0b065fbc96f5963a8d5aac0d8c8498e61
UOC-JS <dan.cassidy@cumbria.ac.uk>
2026-07-09 10:27:55 +0100

    Update js_test.js

--- js_test.js
+++ js_test.js
@@ -32,3 +32,19 @@ console.log("Custom 123FormBuilder script loaded");
     });
   });
 })();
+
+(function () {
+  document.addEventListener("DOMContentLoaded", function () {
+    const params = new URLSearchParams(window.location.search);
+    const los = params.get("los");
+
+    if (!los) return;
+
+    const losField = document.querySelector('select[name="Level of Study"]');
+
+    if (losField) {
+      losField.value = los;
+      losField.dispatchEvent(new Event("change", { bubbles: true }));
+    }
+  });
+})();
